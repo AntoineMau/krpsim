5d0de41bbb8061a2f96d417625126ce27efac4a1
AntoineMau <antoinemauffret@gmail.com>
2022-08-23 15:47:08 +0200

    add class process + finish parsing

--- krpsim.py
+++ krpsim.py
@@ -1,43 +1,63 @@
-from re import sub, match
+from re import sub, match, findall
 from argparse import ArgumentParser, FileType
 from utils import error
 
+class Process:
+	def __init__(self, line):
+		self.name = str()
+		self.need = list()
+		self.result = list()
+		self.delay = int()
+		self.save(line)
+
+	def save(self, line):
+		match1 = match(r'\w+', line)
+		self.name = match1.group(0)
+		match2 = findall(r'(\((\w+:\d+;?)+\))?:', line)
+		need = match2[1][0]
+		for elt in sub(r'[\(\)]', '', need).split(';'):
+			self.need.append(elt.split(':'))
+		result = match2[2][0]
+		for elt in sub(r'[\(\)]', '', result).split(';'):
+			self.result.append(elt.split(':'))
+		match3 = findall(r':\d+$', line)
+		self.delay = int(match3[0][1:])
+
+	def print(self):
+		print('name: %s' % self.name)
+		print('need: %s' % self.need)
+		print('result: %s' % self.result)
+		print('delay: %s\n' % self.delay)
+
 class Krpsim:
 	def __init__(self):
-		self.cycle = int()
+		self.maxCycle = int()
 		self.stock = list()
+		self.lstProcess = list()
+		self.optimize = str()
 
 	def parser(self):
 		parser = ArgumentParser()
 		parser.add_argument('file', type=FileType('r'), help='file to process')
-		parser.add_argument('-C', '--cycle', default=100, help='number of cycle. default:100')
+		parser.add_argument('delay', type=int, help='max time to process')
+		parser.add_argument('-c', '--cycle', default=10000, help='max number of cycle. default:10000')
 		args = parser.parse_args()
-		self.cycle = args.cycle
+		self.maxCycle = args.cycle
 		f = args.file.read()
 		f = sub(r'#.*', '', f)
 
-		### REGEX GOOD I THINK ### (stock would be fine but need to try it and didn't do processus and optimize)
 		for elt in f.split('\n'):
-			if match(r'^[a-z]+:\d+$', elt):
+			if elt == '\n' or elt == '':
+				pass
+			elif match(r'^\w+:\d+$', elt):
 				self.stock.append(elt.split(':'))
-			elif match(r'^[a-z_]+:\(([a-z_]+:\d;?)+\):\(([a-z_]+:\d;?)+\):\d+$', elt):
-				print('match processus')
-			elif match(r'^optimize:\(time;[a-z_]+\)$', elt):
-				print('match optimize')
+			elif match(r'^\w+:(\((\w+:\d+;?)+\))?:(\((\w+:\d+;?)+\))?:\d+$', elt):
+				self.lstProcess.append(Process(elt))
+			elif match(r'^optimize:\((\w+;?)+\)$', elt):
+				self.optimize = findall(r'\w+\)$', elt)[0][:-1]
 			else:
 				error('bad_file')
-			print(elt)
-		### END ###
-		
-		# run parser file
-		### START TRY ###
-		self.stock.append(['client_content', 1])
-		self.stock.append(['produit', 0])
-		self.stock.append(['materiel', 0])
-		self.stock.append(['euro', 2])
-		print('Nice file ! 3 processes, 4 stocks, 1 to optimize')
-		### END ###
-	
+
 	def process(self):
 		### START TRY ###
 		print('Evaluating .................. done.')
@@ -51,7 +71,7 @@ class Krpsim:
 	def print(self):
 		print('Stock :')
 		for elt in self.stock:
-			print('%s => %d' % (elt[0], elt[1]))
+			print('%s => %s' % (elt[0], elt[1]))
 
 def main():
 	krpsim = Krpsim()
